036494c5940dcd1b2406f7fb5a368668331a06dd
michelelagreca <michelelagreca.work@gmail.com>
2024-06-25 11:21:00 +0200

    sig zag active vision pipeline works

--- fruit_picking_nbv/src/nbv_pipeline.cpp
+++ fruit_picking_nbv/src/nbv_pipeline.cpp
@@ -148,6 +148,7 @@ namespace nbv_pipeline{
     void NBVPipeline::NBVPipelineThread(){
         
         RCLCPP_INFO(this->get_logger(), "---------------------------------------");
+        RCLCPP_INFO(this->get_logger(), "---------------------------------------");
         RCLCPP_INFO(this->get_logger(), "NBV pipeline started.");
         // set the rate for the main thread
 	    rclcpp::Rate rate(15);
@@ -203,129 +204,8 @@ namespace nbv_pipeline{
         for (size_t i = 1; i < ZigZagPlanningPoses_.size(); ++i) {
 
             RCLCPP_INFO(this->get_logger(), "---------------------------------------");
+            RCLCPP_INFO(this->get_logger(), "---------------------------------------");
             RCLCPP_INFO(this->get_logger(), "NBV pipeline step started.");
-
-            // // Obtain data from the robot
-
-            // // Lock the variables till the function terminates, locking also the subsciber to save the current data
-            // std::unique_lock<std::mutex> lock(data_mutex_);
-            // // Wait intill notification: then check the value of data received
-            // data_cond_.wait(lock, [this]{ return this->data_received_; });
-            // data_received_ = false; // Reset the flag
-
-            // // Ontain data to send it to the segmentation server
-            // RCLCPP_INFO(this->get_logger(), "Getting data from subscriber...");
-
-            // Image::ConstSharedPtr working_rgb_msg;
-            // Image::ConstSharedPtr working_depth_msg;
-            // CameraInfo::ConstSharedPtr working_camera_info_msg;
-            // geometry_msgs::msg::TransformStamped::ConstSharedPtr working_tf;
-
-            // std::tie(
-            //     working_rgb_msg, working_depth_msg, working_camera_info_msg, working_tf) = std::make_tuple(
-            //     current_rgb_msg_, current_depth_msg_, current_camera_info_msg_, current_tf_);
-
-            // RCLCPP_INFO(this->get_logger(), "Data obtained.");
-
-
-
-
-            // // Create segmentation request for the server
-
-            // auto request = std::make_shared<fruit_picking_interfaces::srv::YOLOWorldSegmentation::Request>();
-            // request->image = *working_rgb_msg;
-            // request->text_prompt = this->prompt_;
-            // request->confidence_threshold = this->confidence_threshold_;
-            // request->nms_threshold = this->nms_confidence_threshold_;
-
-
-
-
-            // // Wait for the server to be active
-
-            // while (!this->client_->wait_for_service(std::chrono::seconds(1))) {
-            //     if (!rclcpp::ok()) {
-            //         RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-            //         return;
-            //     }
-            //     RCLCPP_WARN(this->get_logger(), "Service not available, waiting again...");
-            // }
-
-            // auto result = client_->async_send_request(request);
-
-
-
-
-            // // Wait for the response, and save them once received
-
-            // if (rclcpp::spin_until_future_complete(client_node_, result) ==
-            //     rclcpp::FutureReturnCode::SUCCESS)
-            // {
-            //     auto response = result.get();
-            //     masks_images_array_ = std::make_shared<const ImageArray>(response->masks_images_array);
-            //     merged_masks_image_ = std::make_shared<const Image>(response->merged_masks_images);
-            //     confidences_ = std::make_shared<const Confidence>(response->confidences);
-
-            //     RCLCPP_INFO(this->get_logger(), "Response obtained.");
-            
-            // }
-
-
-            // // Publish segmented image
-            // segmentedImagePub_->publish(*merged_masks_image_);
-
-
-
-
-            // // Create full and segmented pointcloud
-            // if (usePartialPointcloud_){
-            //     RCLCPP_INFO(this->get_logger(), "Creating partial pointcloud...");
-            //     partialPointcloud_ = segmented_pointcloud_node_->imageCb(
-            //         working_depth_msg, 
-            //         merged_masks_image_, 
-            //         working_camera_info_msg);
-            // } 
-            // else {
-            //     RCLCPP_INFO(this->get_logger(), "Creating full pointcloud...");
-            //     fullPointcloud_ = pointcloud_node_->imageCb(
-            //         working_depth_msg, 
-            //         working_rgb_msg, 
-            //         working_camera_info_msg);
-            // }
-            // RCLCPP_INFO(this->get_logger(), "Creating segmented pointclouds array...");
-            
-            // segmentedPointcloudArray_ = segmented_pointcloud_node_->imageArrayCb(
-            //     working_depth_msg,
-            //     masks_images_array_,
-            //     working_camera_info_msg,
-            //     confidences_);
-            // RCLCPP_INFO(this->get_logger(), "Pointclouds created.");
-
-
-
-            // // Publish full and segmented pointclouds for visualization
-            // segmentedPointcloud_ = segmented_pointcloud_node_->imageCb(
-            //     working_depth_msg, 
-            //     merged_masks_image_, 
-            //     working_camera_info_msg);
-            // segmentedPointcloudPub_->publish(*segmentedPointcloud_);
-
-            
-
-
-            // // Update octomap and publish visualization
-            // RCLCPP_INFO(this->get_logger(), "Updating octomap...");
-            // if (usePartialPointcloud_){
-            //     extended_octomap_node_->insertPartialCloudCallback(partialPointcloud_, working_tf);
-            //     extended_octomap_node_->insertSegmentedPointcloudsArrayCallback(segmentedPointcloudArray_, working_tf, partialPointcloud_);
-            // }
-            // else {
-            //     extended_octomap_node_->insertCloudCallback(fullPointcloud_);
-            //     extended_octomap_node_->insertSegmentedPointcloudsArrayCallback(segmentedPointcloudArray_, working_tf, fullPointcloud_);
-            // }
-            // RCLCPP_INFO(this->get_logger(), "Octomap updated.");
-
-
 
 
             // Move to the next position
@@ -347,10 +227,140 @@ namespace nbv_pipeline{
 
 
 
+            // Obtain data from the robot
+
+            // Lock the variables till the function terminates, locking also the subsciber to save the current data
+            std::unique_lock<std::mutex> lock(data_mutex_);
+            // Wait intill notification: then check the value of data received
+            data_cond_.wait(lock, [this]{ return this->data_received_; });
+            data_received_ = false; // Reset the flag
+
+            // Ontain data to send it to the segmentation server
+            RCLCPP_INFO(this->get_logger(), "---------------------------------------");
+            RCLCPP_INFO(this->get_logger(), "Getting data from subscriber...");
+
+            Image::ConstSharedPtr working_rgb_msg;
+            Image::ConstSharedPtr working_depth_msg;
+            CameraInfo::ConstSharedPtr working_camera_info_msg;
+            geometry_msgs::msg::TransformStamped::ConstSharedPtr working_tf;
+
+            std::tie(
+                working_rgb_msg, working_depth_msg, working_camera_info_msg, working_tf) = std::make_tuple(
+                current_rgb_msg_, current_depth_msg_, current_camera_info_msg_, current_tf_);
+
+            RCLCPP_INFO(this->get_logger(), "Data obtained.");
+
+
+
+
+            // Create segmentation request for the server
+
+            auto request = std::make_shared<fruit_picking_interfaces::srv::YOLOWorldSegmentation::Request>();
+            request->image = *working_rgb_msg;
+            request->text_prompt = this->prompt_;
+            request->confidence_threshold = this->confidence_threshold_;
+            request->nms_threshold = this->nms_confidence_threshold_;
+
+
+
+
+            // Wait for the server to be active
+
+            while (!this->client_->wait_for_service(std::chrono::seconds(1))) {
+                if (!rclcpp::ok()) {
+                    RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+                    return;
+                }
+                RCLCPP_WARN(this->get_logger(), "Service not available, waiting again...");
+            }
+
+            auto result = client_->async_send_request(request);
+
+
+
+
+            // Wait for the response, and save them once received
+
+            if (rclcpp::spin_until_future_complete(client_node_, result) ==
+                rclcpp::FutureReturnCode::SUCCESS)
+            {
+                auto response = result.get();
+                masks_images_array_ = std::make_shared<const ImageArray>(response->masks_images_array);
+                merged_masks_image_ = std::make_shared<const Image>(response->merged_masks_images);
+                confidences_ = std::make_shared<const Confidence>(response->confidences);
+
+                RCLCPP_INFO(this->get_logger(), "Response obtained.");
+            
+            }
+
+
+            // Publish segmented image
+            segmentedImagePub_->publish(*merged_masks_image_);
+
+
+            // Introduce a delay
+            //rclcpp::sleep_for(std::chrono::milliseconds(2000));
+
+
+
+            // Create full and segmented pointcloud
+            if (usePartialPointcloud_){
+                RCLCPP_INFO(this->get_logger(), "Creating partial pointcloud...");
+                partialPointcloud_ = segmented_pointcloud_node_->imageCb(
+                    working_depth_msg, 
+                    merged_masks_image_, 
+                    working_camera_info_msg);
+            } 
+            else {
+                RCLCPP_INFO(this->get_logger(), "Creating full pointcloud...");
+                fullPointcloud_ = pointcloud_node_->imageCb(
+                    working_depth_msg, 
+                    working_rgb_msg, 
+                    working_camera_info_msg);
+            }
+            RCLCPP_INFO(this->get_logger(), "Creating segmented pointclouds array...");
+            
+            segmentedPointcloudArray_ = segmented_pointcloud_node_->imageArrayCb(
+                working_depth_msg,
+                masks_images_array_,
+                working_camera_info_msg,
+                confidences_);
+            RCLCPP_INFO(this->get_logger(), "Pointclouds created.");
+
+
+
+            // Publish full and segmented pointclouds for visualization
+            segmentedPointcloud_ = segmented_pointcloud_node_->imageCb(
+                working_depth_msg, 
+                merged_masks_image_, 
+                working_camera_info_msg);
+            segmentedPointcloudPub_->publish(*segmentedPointcloud_);
+
+            
+
+
+            // Update octomap and publish visualization
+            RCLCPP_INFO(this->get_logger(), "Updating octomap...");
+            if (usePartialPointcloud_){
+                extended_octomap_node_->insertPartialCloudCallback(partialPointcloud_, working_tf);
+                extended_octomap_node_->insertSegmentedPointcloudsArrayCallback(segmentedPointcloudArray_, working_tf, partialPointcloud_);
+            }
+            else {
+                extended_octomap_node_->insertCloudCallback(fullPointcloud_);
+                extended_octomap_node_->insertSegmentedPointcloudsArrayCallback(segmentedPointcloudArray_, working_tf, fullPointcloud_);
+            }
+            RCLCPP_INFO(this->get_logger(), "Octomap updated.");
+
+
+
+
             RCLCPP_WARN(this->get_logger(), "NBV pipeline step terminated.");
 
 
         }
+
+        RCLCPP_WARN(this->get_logger(), "NBV pipeline terminated.");
+        RCLCPP_WARN(this->get_logger(), "---------------------------------------");
 
 
     }
